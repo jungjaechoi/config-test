d38013149aee1daadbf4baaea86b67850bf65888
jungjae.choi <www898975@gmail.com>
2022-08-12 16:37:13 +0900

    test

--- src/main/java/com/example/configuration/ConfigurationApplication.java
+++ src/main/java/com/example/configuration/ConfigurationApplication.java
@@ -11,7 +11,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import java.io.InputStream;
 
 @SpringBootApplication
-@EnableSwagger2
 public class ConfigurationApplication {
 
 	public static void main(String[] args) {
